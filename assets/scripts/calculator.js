8828ce36e380e7a696d350877074893be18df8a5
Eric <ericscottlagergren@gmail.com>
2014-06-10 20:30:31 -0700

    k

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -2,11 +2,17 @@ function val() {
 
 gallons = document.form.a.value;
 monthly = document.form.b.value;
-now = (3.899 * gallons) * monthly;
-inslee = ((3.899 + 1.17) * gallons) * monthly;
+now = ((3.899 * gallons) * monthly) * 1.10;
+inslee = (((3.899 + 1.17) * gallons) * monthly) * 1.10;
 change = inslee - now;
 changeFixed = change.toFixed(2);
 year = change * 12;
+
+// 3.899 is the average price for gas in WA right now
+// 1.17 is how much Inslee's plan is supposed to increase prices by per gallon
+// Biofuels have about 10% less power, so you'll end up needing to buy 10% more to make up for it
+
+
 
 document.getElementById("current").innerHTML = "$" + now.toFixed(2);
 document.getElementById("future").innerHTML = "$" + inslee.toFixed(2);
